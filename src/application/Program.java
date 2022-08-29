ef5df2cade6957b361e3a56fd22f13e9c22294aa
FullStackDevil <hique.rocha@gmail.com>
2022-08-29 04:15:03 -0300

    Impl: findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,22 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(1, "Books");
+//		Department department = new Department(1, "Books");
+//
+//		Seller seller = new Seller(21, "Grog", "grog@gmail.com", new Date(), 3000.0, department);
 
-		Seller seller = new Seller(21, "Grog", "grog@gmail.com", new Date(), 3000.0, department);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(sellerDao);
+
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
 
 	}
 
