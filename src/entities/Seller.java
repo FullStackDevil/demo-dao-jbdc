bc65136f114c59f948aea26aa0acefb778db1eb4
FullStackDevil <hique.rocha@gmail.com>
2022-08-29 03:13:01 -0300

    Seller entity class

--- src/entities/Seller.java
+++ src/entities/Seller.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Seller {
 
@@ -71,6 +72,29 @@ public class Seller {
 
 	public void setDepartment(Department department) {
 		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
 	}
 
 }
